fbafae4181fa098f051d76890b77f83aa8d167f0
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-13 16:17:59 -0400

    2-2-2 Router works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   );
 }
