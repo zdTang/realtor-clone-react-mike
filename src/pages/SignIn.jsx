247abc4c097d72b1464f8665a51e6b4948800d29
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-19 19:13:11 -0400

    3-1-4 Add flex to change the layout

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,15 +4,19 @@ function SignIn() {
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
-      <div>
+      
+      <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
+        
         <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
           <img src="images/signin.jpg" alt="signIn" className='w-full rounded-2xl'/>
         </div>
+        
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form >
-            <input className='w-full' type="text"></input>
+          <form>
+            <input className='w-full' type="text" placeholder='email address'></input>
           </form>
         </div>
+
       </div>
     </section>
   )
