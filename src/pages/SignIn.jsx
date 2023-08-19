55877396347a6e1393cbfa26cc2f03c192b98b70
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-19 18:40:10 -0400

    3-1-1 Create SignIn layout and add Image

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 function SignIn() {
   return (
-    <div>
-      <h1>SignIn</h1>
-    </div>
+    <section>
+      <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
+      <div>
+        <div>
+          <img src="images/signin.jpg" alt="signIn"/>
+        </div>
+        <div></div>
+      </div>
+    </section>
   )
 }
 
