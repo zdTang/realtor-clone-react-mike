b27adda8ef7c4dae15b0683a2028d7aeea9e7457
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-19 18:50:01 -0400

    3-1-2 Style the image and make it reactive

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -5,8 +5,8 @@ function SignIn() {
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
       <div>
-        <div>
-          <img src="images/signin.jpg" alt="signIn"/>
+        <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
+          <img src="images/signin.jpg" alt="signIn" className='w-full rounded-2xl'/>
         </div>
         <div></div>
       </div>
