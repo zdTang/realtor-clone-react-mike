2188f30c3e31f3950d9394b9cef664e988fa3625
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-20 05:32:22 -0400

    3-1-5 Install Tailwindcss/forms package, to make Form looks better

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,25 +1,62 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
 function SignIn() {
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
   return (
     <section>
       <h1 className="text-3xl text-center mt-6 font-bold">Sign In</h1>
-      
-      <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
-        
-        <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
-          <img src="images/signin.jpg" alt="signIn" className='w-full rounded-2xl'/>
+
+      <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
+        <div className="md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
+          <img
+            src="images/signin.jpg"
+            alt="signIn"
+            className="w-full rounded-2xl"
+          />
         </div>
-        
-        <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
+
+        <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
           <form>
-            <input className='w-full' type="text" placeholder='email address'></input>
+
+            <div>
+              <input
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+                id="email"
+                type="email"
+                value={email}
+                placeholder="Email address"
+                onChange={onChange}
+              ></input>
+            </div>
+
+            <div>
+              <input
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
+                id="password"
+                type={showPassword ? "text" : "password"} // if showPassword is true, show text, else show password
+                value={password}
+                placeholder="Password"
+                onChange={onChange}
+              ></input>
+            </div>
+            
           </form>
         </div>
-
       </div>
     </section>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
