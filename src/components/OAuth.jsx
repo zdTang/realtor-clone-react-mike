e88259aa19e221051d48962eb344b44d98712286
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-23 11:05:23 -0400

    3-3-3 Add try-catch and use Toastify

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -5,29 +5,35 @@ import { getAuth } from "firebase/auth";
 import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
 import { db } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
 export default function OAuth() {
 
   const navigate = useNavigate();
   // Here we will use Firebase google authentication
   async function onGoogleClick(){
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-
-    // check for the user
-    const docRef = doc(db, "users", user.uid);   // get the doc reference
-    const docSnap = await getDoc(docRef);        // get the doc snapshot
-
-    if (!docSnap.exists()) {
-      await setDoc(docRef, {                   // if the user does not exist, create a new user
-        name: user.displayName,
-        email: user.email,
-        timestamp: serverTimestamp(),
-      });
+    try{
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+  
+      // check for the user
+      const docRef = doc(db, "users", user.uid);   // get the doc reference
+      const docSnap = await getDoc(docRef);        // get the doc snapshot
+  
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {                   // if the user does not exist, create a new user
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        });
+      } 
+      navigate("/");
     }
-
-    navigate("/");
+    catch(error){
+      toast.error("Could not authorize with Google");
+    }
   }
 
   return (
