d48165db34ff22526cd6ae7012f9547e09451d1c
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-24 11:09:47 -0400

    4-1-2 Finish profile form UI part

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -24,10 +24,16 @@ function Profile() {
       <div className='w-full md:w-[50%] mt-6 px-3'>
         <form>
           <input type="text" id='name' placeholder='Name' value={name} disabled 
-          className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"/>
+          className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6"/>
+           <input type="text" id='email' placeholder='Email' value={email} disabled 
+          className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out mb-6"/>
+        <div className='flex justify-between whitespace-nowrap text-sm sm:text-lg px-3 mb-6'>
+          <p>Do you want to change your name? <strong className='text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer'>Edit </strong></p>
+          <p className='text-blue-600 hover:text-blue-700 transition ease-in-out duration-200 cursor-pointer'>Sign out</p>
+        </div>
         </form>
       </div>
-    </section> 
+    </section>  
   )
 }
 
