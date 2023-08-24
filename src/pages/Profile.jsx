e6aa2b99253426cf7cc68f4dcc5cf48c985d0012
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-24 10:39:32 -0400

    4-1-1 Add name input box and style

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { useState } from 'react'  
 
 function Profile() {
+  const [formData, setFormData] =  React.useState({
+    name: '',
+    email:"example@gmail.com"
+  });
+
+  const { name, email } = formData;
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+  }
+
+
+
+  // const handleSubmit = (e
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
+      <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
+      <div className='w-full md:w-[50%] mt-6 px-3'>
+        <form>
+          <input type="text" id='name' placeholder='Name' value={name} disabled 
+          className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"/>
+        </form>
+      </div>
+    </section> 
   )
 }
 
