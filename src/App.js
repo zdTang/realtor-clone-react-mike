9d21e60b1d3fb081bb57c437835a2cb8e29accbc
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-26 11:26:56 -0400

    4-5-2 Add "create-listing" to the Router

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Header from "./components/Header";
-import React from 'react';
-import { ToastContainer } from 'react-toastify';
+import React from "react";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; // Never forget to import this
 import PrivateRoute from "./components/PrivateRoute";
-
+import CreateListing from "./pages/CreateListing";
 
 function App() {
   console.log("into App--");
   return (
     <>
-        <Router>
+      <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -24,26 +24,27 @@ function App() {
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
-          
+
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/create-listing" element={<CreateListing />} />
         </Routes>
       </Router>
 
-        <ToastContainer
-          position="bottom-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="dark"
-        />
+      <ToastContainer
+        position="bottom-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </>
   );
 }
