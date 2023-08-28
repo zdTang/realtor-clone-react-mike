8f05da748a525ccc55b864e74b11fafa5859572e
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-28 05:55:15 -0400

    4-5-3 Create Partial UI components

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 
 function CreateListing() {
-  return <div>CreateListing</div>;
+
+    const [formData, setFormData] = React.useState({ type: "rent" });
+    const { type } = formData;
+    
+    function onChange(e) {
+    console.log(e.target.value)
+    }
+    
+  return (
+    <main className="max-w-md px-2 mx-auto">
+          <h1 className="text-3xl text-center mt-6 font-bold">Create a Listing</h1>
+          <form>
+              <p className="text-lg mt-6 font-semibold">Sell/Rent</p>
+              <div>
+                  <button
+                      type="button"
+                      id="sell"
+                      value={type}
+                      onClick={onChange}
+                      className={`px-7 py-3 font-medium text-sm uppercase shadow-md rounded hover:shadow-lg 
+                      focus:shadow-lg active:shadow-lg transition duration-150 ease-in-out w-full
+                      ${type==="rent" ? "bg-white":"bg-slate-600"}`}
+                  >   
+                      SELL
+                  </button>
+                  <button>RENT</button>
+              </div>
+          </form>
+    </main>
+  );
 }
 
 export default CreateListing;
