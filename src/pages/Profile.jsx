5031b938405a31751f20c9a45d7dc3f90bc4ca35
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-08-30 20:02:12 -0400

    4-8. Complete the listingItem component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -22,7 +22,7 @@ function Profile() {
   const navigate = useNavigate();
   const [changeDetail, setChangeDetail] = useState(false);
   const [listings, setListings] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);  //if it is loading the Spinner
   console.log("Profile--changeDetail:", changeDetail);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
@@ -164,8 +164,8 @@ function Profile() {
       <div className="max-w-6xl px-3 mt-6 mx-auto">
         {!loading && listings.length > 0 && (
           <>
-            <h2 className="text-2xl text-center font-semibold">My Listings</h2>
-            <ul>
+            <h2 className="text-2xl text-center font-semibold mb-6">My Listings</h2>
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
               {listings.map((listing) => (
                 <ListingItem
                   key={listing.id}
