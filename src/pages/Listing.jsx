46565d5c42320e89b687b02ec20c7fc806d0c254
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2023-09-01 18:03:03 -0400

    5-1-2 Create listing page and use Swiper to add the image slider
    update to Swiper 10 other than 8

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import { doc, getDoc } from "firebase/firestore";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import Spinner from "../components/Spinner";
+import { db } from "../firebase";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper/modules";
+import "swiper/css";
 
 export default function Listing() {
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  //SwiperCore.use([Autoplay, Navigation, Pagination]);
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.id);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    }
+    fetchListing();
+  }, [params.id]);
+  if (loading) {
+    return <Spinner />;
+  }
   return (
-    <div>Listing</div>
-  )
+    <main>
+      <Swiper
+        modules={[EffectFade,Autoplay,Navigation,Pagination]}
+        //spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        //scrollbar={{ draggable: true }}
+        autoplay={{ delay: 3000 }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="relative w-full overflow-hidden h-[300px]"
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </main>
+  );
 }
+
